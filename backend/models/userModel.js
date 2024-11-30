037d80694f53972cee6d6d47c66c56d0018f61e2
Mohammad Shariful Islam <mohammadsharifulislam64@gmail.com>
2024-11-30 23:57:59 +0600

    create login api

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -37,5 +37,20 @@ userSchema.statics.signup = async function (email, password) {
     const user = await this.create({ email, password: hash });
     return user;
 }
+// static login message
+userSchema.statics.login = async function (email, password) {
+    if (!email || !password) {
+        throw new Error('Email and password are required');
+    }
+    const user = await this.findOne({ email });
+    if (!user) {
+        throw new Error('Incorrect email');
+    }
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+        throw new Error('Incorrect password');
+    }
+    return user;
+}
 
 module.exports = mongoose.model('User', userSchema);
