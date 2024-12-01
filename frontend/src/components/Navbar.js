ae3b7acb49c8796aeb2d44dcdc560243bc9c9874
Mohammad Shariful Islam <mohammadsharifulislam64@gmail.com>
2024-12-01 23:47:45 +0600

    state initialize auth state

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,8 +1,11 @@
 import { Link } from 'react-router-dom'
 import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext';
 
 const Navbar = () => {
-  const { logout } = useLogout()
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
+
 
   return (
     <header>
@@ -12,11 +15,12 @@ const Navbar = () => {
         </Link>
         <nav>
           <div style={{ display: 'flex', alignItems: 'center', gap: '4px' }}>
-            <button onClick={() => {
+            <Link to='/'>Home</Link>
+            {user?.email ? <button style={{ cursor: 'pointer' }} onClick={() => {
               logout()
-            }}>Log Out</button>
-            <Link to='/login'>Login</Link>
-            <Link to='/signup'>Signup</Link>
+            }}><span>{user?.email}</span> Log Out</button> : <><Link to='/login'>Login</Link>
+              <Link to='/signup'>Signup</Link>
+            </>}
           </div>
         </nav>
       </div>
